56d9d6cb09e386b74e562025a2b6d556a3618927
aesakof <aesakof@gmail.com>
2023-01-08 16:20:13 -0500

    Django Initialization

---
+++ django-backend/recipe_app_api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import FillupList, FillupDetail, CarList, CarDetail
+
+app_name = 'recipe_app_api'
+
+urlpatterns = [
+    # path('<int:pk>/', CarDetail.as_view(), name='cardetailcreate'),
+    # path('', CarList.as_view(), name='carlistcreate'),
+]
