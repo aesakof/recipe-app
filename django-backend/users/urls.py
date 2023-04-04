4437da38491acf2f645365df691838e91ba6af20
aesakof <aesakof@gmail.com>
2023-04-03 23:16:16 -0400

    Backend checks for existing usernames and emails

--- django-backend/users/urls.py
+++ django-backend/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import CheckAuthUser, CustomUserCreate, BlacklistTokenUpdateView
+from .views import CheckAuthUser, CheckEmailExists, CheckUsernameExists, CustomUserCreate, BlacklistTokenUpdateView
 
 app_name = 'users'
 
 urlpatterns = [
     path('create/', CustomUserCreate.as_view(), name="create_user"),
     path('logout/blacklist/', BlacklistTokenUpdateView.as_view(), name='blacklist'),
-    path('checkauthuser/', CheckAuthUser, name="checkauthuser")
+    path('checkauthuser/', CheckAuthUser, name="checkauthuser"),
+    path('checkemailexists/', CheckEmailExists, name="checkemailexists"),
+    path('checkusernameexists/', CheckUsernameExists, name="checkusernameexists"),
 ]
--- django-backend/users/views.py
+++ django-backend/users/views.py
@@ -1,11 +1,12 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from .serializers import CustomUserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny, IsAdminUser
+from .models import AppUser
 
 
 @api_view()
@@ -19,6 +20,42 @@ def CheckAuthUser(request):
         return Response({
             "message": "Not authenticated",
         })
+
+
+@api_view(['POST'])
+# @permission_classes([AllowAny])
+def CheckEmailExists(request):
+    email = request.data.get('email')
+    if AppUser.objects.filter(email=email):
+        return Response({
+            "isAvailable": "false"
+        })
+    else:
+        return Response({
+            "isAvailable": "true"
+        })
+
+
+@api_view(['POST'])
+def CheckUsernameExists(request):
+    user_name = request.data.get('user_name')
+    if AppUser.objects.filter(user_name=user_name):
+        return Response({
+            "isAvailable": "false"
+        })
+    else:
+        return Response({
+            "isAvailable": "true"
+        })
+
+
+class AppUserViewset(viewsets.ModelViewSet):
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']: # also retrieve
+            self.permission_classes = [IsAdminUser]
+        else:
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
 
 
 class CustomUserCreate(APIView):
